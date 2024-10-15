db71e8c78f258bf9c0b49b1cb38357193637201b
KUCA Brice <kuca.b@sfeir.com>
2024-10-15 09:31:57 +0200

    d

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const admin = require('firebase-admin');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World! coucou hibou');
