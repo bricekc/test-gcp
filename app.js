29185e2dca21a5b09e2eaa712b372908b816f294
KUCA Brice <kuca.b@sfeir.com>
2024-10-15 09:25:09 +0200

    feat: test

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ const port = 8080;
 app.get('/', (req, res) => {
   res.send('Hello World! coucou');
 });
+admin.initializeApp();
+console.log('Firebase initialisé sur Cloud Run avec l\'authentification par défaut');
 
+
+// Référence à la base de données Firestore
+const db = admin.firestore();
+
+// Route pour créer un document dans la collection "users"
+app.post('/api/users', async (req, res) => {
+  try {
+    const user = req.body;
+    const docRef = await db.collection('users').add(user);
+    res.status(201).send({ id: docRef.id, ...user });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
